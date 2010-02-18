dedb47f897f58ca407f1dee066c1e00c478bc60e
Henrik Paul <wolfie@Henrik-Pauls-MacBook-Pro-15.local>
2010-02-18 22:14:47 +0200

    Version 0.0.1

---
+++ src/com/github/wolfie/disconnectedtabs/DetachedTabs.java
@@ -0,0 +1,191 @@
+package com.github.wolfie.disconnectedtabs;
+
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
+import com.vaadin.ui.AbstractOrderedLayout;
+import com.vaadin.ui.Button;
+import com.vaadin.ui.Component;
+import com.vaadin.ui.ComponentContainer;
+import com.vaadin.ui.CustomComponent;
+import com.vaadin.ui.HorizontalLayout;
+import com.vaadin.ui.NativeButton;
+import com.vaadin.ui.TabSheet;
+import com.vaadin.ui.VerticalLayout;
+import com.vaadin.ui.Button.ClickEvent;
+import com.vaadin.ui.Button.ClickListener;
+
+/**
+ * <p>
+ * A sort of {@link TabSheet}, with the tabs detached from its sheet.
+ * </p>
+ * 
+ * <p>
+ * This component is merely the tabs for a tabsheet. The sheet itself is an
+ * external object. These two may be placed anywhere, independent of each other.
+ * </p>
+ * 
+ * <p>
+ * This mechanism allows for a more flexible layouts, and you may place other
+ * widgets right alongside it, and are not constrained by the "bounding box"
+ * presented by the <tt>TabSheet</tt>'s tabs+sheet combination.
+ * </p>
+ * 
+ * @author Henrik Paul
+ * @license Apache 2.0
+ */
+@SuppressWarnings("serial")
+public abstract class DetachedTabs extends CustomComponent {
+  
+  public static class Horizontal extends DetachedTabs {
+    /**
+     * Creates a new horizontal {@link DetachedTabs}.
+     * 
+     * @param componentContainer
+     *          The {@link ComponentContainer} that will act as
+     *          "the sheet for the tabs". The client code will lose all control
+     *          over the <tt>componentContainer</tt>, excepting where it will be
+     *          added. So, you may add it to any layout you want, but you are
+     *          not in control over what will be presented inside it anymore.
+     */
+    public Horizontal(final ComponentContainer componentContainer) {
+      super(componentContainer, Orientation.HORIZONTAL);
+    }
+  }
+  
+  public static class Vertical extends DetachedTabs {
+    /**
+     * Creates a new vertical {@link DetachedTabs}.
+     * 
+     * @param componentContainer
+     *          The {@link ComponentContainer} that will act as
+     *          "the sheet for the tabs". The client code will lose all control
+     *          over the <tt>componentContainer</tt>, excepting where it will be
+     *          added. So, you may add it to any layout you want, but you are
+     *          not in control over what will be presented inside it anymore.
+     */
+    public Vertical(final ComponentContainer componentContainer) {
+      super(componentContainer, Orientation.VERTICAL);
+    }
+  }
+  
+  private class TabChangeListener implements ClickListener {
+    @Override
+    public void buttonClick(final ClickEvent event) {
+      final Button button = event.getButton();
+      final Component componentToSwitch = buttonComponentMap.get(button);
+      switchTo(componentToSwitch, button);
+    }
+  }
+  
+  public enum Orientation {
+    HORIZONTAL, VERTICAL
+  };
+  
+  private final AbstractOrderedLayout layout;
+  private final ComponentContainer componentContainer;
+  private final ClickListener tabChangeListener = new TabChangeListener();
+  private final Map<Button, Component> buttonComponentMap = new LinkedHashMap<Button, Component>();
+  private final List<Button> tabs = new ArrayList<Button>();
+  private Button shownTab = null;
+  private final Orientation orientation;
+  
+  private static final String CLASS = "detachedtabs";
+  private static final String CLASS_HORIZONTAL = CLASS + "-horizontal";
+  private static final String CLASS_VERTICAL = CLASS + "-vertical";
+  
+  private static final String TAB_CLASS = "tab";
+  private static final String TAB_FIRST_CLASS = TAB_CLASS + "-first";
+  private static final String TAB_LAST_CLASS = TAB_CLASS + "-last";
+  private static final String TAB_SELECTED_CLASS = TAB_CLASS + "-selected";
+  
+  private DetachedTabs(final ComponentContainer componentContainer,
+      final Orientation orientation) {
+    
+    if (componentContainer == null || orientation == null) {
+      throw new NullPointerException("arguments may not be null");
+    }
+    
+    setStyleName(CLASS);
+    
+    switch (orientation) {
+    case HORIZONTAL:
+      layout = new HorizontalLayout();
+      addStyleName(CLASS_HORIZONTAL);
+      break;
+    case VERTICAL:
+      layout = new VerticalLayout();
+      addStyleName(CLASS_VERTICAL);
+      break;
+    default:
+      throw new UnsupportedOperationException("orientation " + orientation
+          + " not supported");
+    }
+    
+    setCompositionRoot(layout);
+    this.componentContainer = componentContainer;
+    this.orientation = orientation;
+  }
+  
+  /**
+   * Add a tab
+   * 
+   * @param content
+   *          The {@link Component} that will be shown once its corresponding
+   *          tab is selected.
+   * @param caption
+   *          The caption for the tab.
+   */
+  public void addTab(final Component content, final String caption) {
+    if (content == null || caption == null) {
+      throw new NullPointerException("Arguments may not be null");
+    }
+    
+    final Button button = new NativeButton(caption, tabChangeListener);
+    
+    if (orientation == Orientation.HORIZONTAL) {
+      button.setHeight("100%");
+    } else {
+      button.setWidth("100%");
+    }
+    
+    layout.addComponent(button);
+    buttonComponentMap.put(button, content);
+    tabs.add(button);
+    
+    adjustTabStyles();
+    
+    if (shownTab == null) {
+      switchTo(content, button);
+    }
+  }
+  
+  private void adjustTabStyles() {
+    if (!tabs.isEmpty()) {
+      final Component first = tabs.get(0);
+      Component last = first;
+      
+      for (final Component tab : tabs) {
+        tab.setStyleName(TAB_CLASS);
+        last = tab;
+      }
+      
+      first.addStyleName(TAB_FIRST_CLASS);
+      last.addStyleName(TAB_LAST_CLASS);
+      
+      if (shownTab != null) {
+        shownTab.addStyleName(TAB_SELECTED_CLASS);
+      }
+    }
+  }
+  
+  private void switchTo(final Component componentToSwitch, final Button button) {
+    componentContainer.removeAllComponents();
+    componentContainer.addComponent(componentToSwitch);
+    shownTab = button;
+    
+    adjustTabStyles();
+  }
+}
---
+++ src/com/github/wolfie/disconnectedtabs/DisconnectedtabsApplication.java
@@ -0,0 +1,82 @@
+package com.github.wolfie.disconnectedtabs;
+
+import java.util.Random;
+
+import com.vaadin.Application;
+import com.vaadin.ui.Alignment;
+import com.vaadin.ui.Component;
+import com.vaadin.ui.CssLayout;
+import com.vaadin.ui.HorizontalLayout;
+import com.vaadin.ui.Label;
+import com.vaadin.ui.Table;
+import com.vaadin.ui.Window;
+
+@SuppressWarnings("serial")
+public class DisconnectedtabsApplication extends Application {
+  
+  @Override
+  public void init() {
+    final Window mainWindow = new Window("Disconnectedtabs Application");
+    setMainWindow(mainWindow);
+    mainWindow.setContent(new CssLayout());
+    mainWindow.getContent().setSizeFull();
+    
+    final CssLayout body = new CssLayout();
+    body.setSizeFull();
+    
+    final DetachedTabs detachedTabs = new DetachedTabs.Horizontal(body);
+    
+    final HorizontalLayout header = new HorizontalLayout();
+    final Label logo = new Label("LOGO!");
+    final Label right = new Label("User Name | Logout");
+    header.addComponent(logo);
+    header.addComponent(detachedTabs);
+    header.addComponent(right);
+    header.setWidth("100%");
+    header.setComponentAlignment(right, Alignment.MIDDLE_RIGHT);
+    right.setSizeUndefined();
+    
+    mainWindow.addComponent(header);
+    mainWindow.addComponent(body);
+    
+    detachedTabs.addTab(getRandomTable(), "Tab 1");
+    detachedTabs.addTab(getRandomTable(), "Tab 2");
+    detachedTabs.addTab(getRandomTable(), "Tab 3");
+    detachedTabs.addTab(getVerticalExample(), "Vertical");
+  }
+  
+  private Component getRandomTable() {
+    final Table table = new Table();
+    table.addContainerProperty("#1", Integer.class, null);
+    table.addContainerProperty("#2", Integer.class, null);
+    table.addContainerProperty("#3", Integer.class, null);
+    
+    final Random rand = new Random();
+    for (int i = 0; i < 50; i++) {
+      table.addItem(new Integer[] { rand.nextInt(), rand.nextInt(),
+          rand.nextInt() }, new Object());
+    }
+    
+    table.setSizeFull();
+    return table;
+  }
+  
+  private Component getVerticalExample() {
+    final HorizontalLayout layout = new HorizontalLayout();
+    layout.setSizeFull();
+    
+    final CssLayout cssLayout = new CssLayout();
+    cssLayout.setSizeFull();
+    
+    final DetachedTabs verticalTabs = new DetachedTabs.Vertical(cssLayout);
+    verticalTabs.addTab(getRandomTable(), "Tab 1");
+    verticalTabs.addTab(getRandomTable(), "Tab 2");
+    verticalTabs.addTab(getRandomTable(), "Tab 3");
+    verticalTabs.setWidth("100px");
+    
+    layout.addComponent(verticalTabs);
+    layout.addComponent(cssLayout);
+    layout.setExpandRatio(cssLayout, 1.0f);
+    return layout;
+  }
+}
