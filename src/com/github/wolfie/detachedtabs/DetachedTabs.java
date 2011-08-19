1c34e21014743bb8115a9b7686fb230ec56fdc4b
Henrik Paul <henrik.paul@gmail.com>
2011-08-19 14:37:59 +0300

    updated stuff, recompiled for Java 1.5

--- src/com/github/wolfie/detachedtabs/DetachedTabs.java
+++ src/com/github/wolfie/detachedtabs/DetachedTabs.java
@@ -8,6 +8,8 @@ import java.util.Map;
 
 import com.vaadin.ui.AbstractOrderedLayout;
 import com.vaadin.ui.Button;
+import com.vaadin.ui.Button.ClickEvent;
+import com.vaadin.ui.Button.ClickListener;
 import com.vaadin.ui.Component;
 import com.vaadin.ui.ComponentContainer;
 import com.vaadin.ui.CustomComponent;
@@ -15,8 +17,6 @@ import com.vaadin.ui.HorizontalLayout;
 import com.vaadin.ui.NativeButton;
 import com.vaadin.ui.TabSheet;
 import com.vaadin.ui.VerticalLayout;
-import com.vaadin.ui.Button.ClickEvent;
-import com.vaadin.ui.Button.ClickListener;
 
 /**
  * <p>
@@ -39,7 +39,7 @@ import com.vaadin.ui.Button.ClickListener;
  */
 @SuppressWarnings("serial")
 public abstract class DetachedTabs extends CustomComponent {
-  
+
   public static class Horizontal extends DetachedTabs {
     /**
      * Creates a new horizontal {@link DetachedTabs}.
@@ -55,7 +55,7 @@ public abstract class DetachedTabs extends CustomComponent {
       super(componentContainer, Orientation.HORIZONTAL);
     }
   }
-  
+
   public static class Vertical extends DetachedTabs {
     /**
      * Creates a new vertical {@link DetachedTabs}.
@@ -71,20 +71,19 @@ public abstract class DetachedTabs extends CustomComponent {
       super(componentContainer, Orientation.VERTICAL);
     }
   }
-  
+
   private class TabChangeListener implements ClickListener {
-    @Override
     public void buttonClick(final ClickEvent event) {
       final Button button = event.getButton();
       final Component componentToSwitch = buttonComponentMap.get(button);
       switchTo(componentToSwitch, button);
     }
   }
-  
+
   public enum Orientation {
     HORIZONTAL, VERTICAL
   };
-  
+
   private final AbstractOrderedLayout layout;
   private final ComponentContainer componentContainer;
   private final ClickListener tabChangeListener = new TabChangeListener();
@@ -92,27 +91,27 @@ public abstract class DetachedTabs extends CustomComponent {
   private final List<Button> tabs = new ArrayList<Button>();
   private Button shownTab = null;
   private final Orientation orientation;
-  
+
   private static final String CLASS = "detachedtabs";
   private static final String CLASS_HORIZONTAL = CLASS + "-horizontal";
   private static final String CLASS_VERTICAL = CLASS + "-vertical";
-  
+
   private static final String TAB_CLASS = "tab";
   private static final String TAB_FIRST_CLASS = TAB_CLASS + "-first";
   private static final String TAB_LAST_CLASS = TAB_CLASS + "-last";
   private static final String TAB_SELECTED_CLASS = TAB_CLASS + "-selected";
-  
+
   private DetachedTabs(final ComponentContainer componentContainer,
       final Orientation orientation) {
-    
+
     if (componentContainer == null || orientation == null) {
       throw new NullPointerException("arguments may not be null");
     }
-    
+
     setStyleName(CLASS);
-    
+
     setSizeFull();
-    
+
     switch (orientation) {
     case HORIZONTAL:
       layout = new HorizontalLayout();
@@ -128,14 +127,14 @@ public abstract class DetachedTabs extends CustomComponent {
       throw new UnsupportedOperationException("orientation " + orientation
           + " not supported");
     }
-    
+
     layout.setSizeFull();
-    
+
     setCompositionRoot(layout);
     this.componentContainer = componentContainer;
     this.orientation = orientation;
   }
-  
+
   /**
    * Add a tab
    * 
@@ -149,9 +148,9 @@ public abstract class DetachedTabs extends CustomComponent {
     if (content == null || caption == null) {
       throw new NullPointerException("Arguments may not be null");
     }
-    
+
     final Button button = new NativeButton(caption, tabChangeListener);
-    
+
     if (orientation == Orientation.HORIZONTAL) {
       button.setHeight("100%");
       button.setWidth(getWidth(), getWidthUnits());
@@ -159,45 +158,45 @@ public abstract class DetachedTabs extends CustomComponent {
       button.setHeight(getHeight(), getHeightUnits());
       button.setWidth("100%");
     }
-    
+
     layout.addComponent(button);
     buttonComponentMap.put(button, content);
     tabs.add(button);
-    
+
     adjustTabStyles();
-    
+
     if (shownTab == null) {
       switchTo(content, button);
     }
   }
-  
+
   private void adjustTabStyles() {
     if (!tabs.isEmpty()) {
       final Component first = tabs.get(0);
       Component last = first;
-      
+
       for (final Component tab : tabs) {
         tab.setStyleName(TAB_CLASS);
         last = tab;
       }
-      
+
       first.addStyleName(TAB_FIRST_CLASS);
       last.addStyleName(TAB_LAST_CLASS);
-      
+
       if (shownTab != null) {
         shownTab.addStyleName(TAB_SELECTED_CLASS);
       }
     }
   }
-  
+
   private void switchTo(final Component componentToSwitch, final Button button) {
     componentContainer.removeAllComponents();
     componentContainer.addComponent(componentToSwitch);
     shownTab = button;
-    
+
     adjustTabStyles();
   }
-  
+
   @Override
   public void setWidth(final float width, final int unit) {
     if (orientation == Orientation.VERTICAL) {
@@ -206,10 +205,10 @@ public abstract class DetachedTabs extends CustomComponent {
         i.next().setWidth(width, unit);
       }
     }
-    
+
     super.setWidth(width, unit);
   }
-  
+
   @Override
   public void setHeight(final float height, final int unit) {
     if (orientation == Orientation.HORIZONTAL) {
@@ -218,7 +217,7 @@ public abstract class DetachedTabs extends CustomComponent {
         i.next().setHeight(height, unit);
       }
     }
-    
+
     super.setHeight(height, unit);
   }
 }
